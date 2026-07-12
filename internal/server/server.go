9367de0178d437b27f7867bcf36cf6b6e290ae07
AlexMos1n <alex-mos777@yandex.ru>
2026-07-12 17:23:53 +0300

    first_iteration

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,34 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+type ServerStruct struct {
+	Logger *log.Logger
+	Server *http.Server
+}
+
+func CreateServerMorse(log *log.Logger) *ServerStruct {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlers.HandleMain)
+	mux.HandleFunc("/upload", handlers.HandleUpload)
+
+	currentServer := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ErrorLog:     log,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	return &ServerStruct{
+		Logger: log,
+		Server: currentServer,
+	}
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,30 @@
 package service
+
+import (
+	"errors"
+	"strings"
+	"unicode"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func DecoderMorseCode(message string) (string, error) {
+	code := strings.TrimSpace(message)
+	if code == "" {
+		return "", errors.New("an empty string is received")
+	}
+
+	if getIsMorse(code) {
+		return morse.ToText(code), nil
+	}
+	return morse.ToMorse(code), nil
+}
+
+func getIsMorse(code string) bool {
+	for _, c := range code {
+		if c != '-' && c != '.' && !unicode.IsSpace(c) {
+			return false
+		}
+	}
+	return true
+}
